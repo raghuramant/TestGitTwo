8f8db1640eb4b0a3e896b584efba3f854e4e72a3
unknown <sherinsathiya@gmail.com>
2018-06-27 11:43:26 +0530

    add another methoed

---
+++ testpack/ClassOne.java
@@ -0,0 +1,22 @@
+package testpack;
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		method1();
+	}
+
+	static void method1() {
+		System.out.println("method1");
+	}
+
+	static void method2() {
+		System.out.println("method2");
+	}
+	static void sheri1() {
+		System.out.println("sherin shalija");
+	}
+
+}
